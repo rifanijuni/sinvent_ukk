2f032106f7027409e360608723754a0cdbe41a01
rifanijuni <rifanijuniarti29@gmail.com>
2024-06-13 12:31:26 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\BarangMasukController;
 use App\Http\Controllers\BarangKeluarController;
@@ -15,8 +14,23 @@ Route::get('/', function () {
 });
 
 
-Route::resource('barang', BarangController::class);
-Route::resource('kategori', KategoriController::class);
-Route::resource('barangmasuk', BarangMasukController::class);
-Route::resource('barangkeluar', BarangKeluarController::class);
+Route::resource('barang', BarangController::class)->middleware('auth');
+Route::resource('kategori', KategoriController::class)->middleware('auth');
+Route::resource('barangmasuk', BarangMasukController::class)->middleware('auth');
+Route::resource('barangkeluar', BarangKeluarController::class)->middleware('auth');
 
+Route::get('login', [LoginController::class,'index'])->name('login')->middleware('guest');
+Route::post('login', [LoginController::class,'authenticate']);
+Route::post('logout', [LoginController::class,'logout']);
+Route::get('logout', [LoginController::class,'logout']);
+
+Route::get('register', [RegisterController::class,'create'])->name('register');
+Route::post('register', [RegisterController::class,'store']);
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
